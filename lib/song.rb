187eef6ec1671c52fbd328bbecbc6153a73da2ed
Sharmina Akhtar <sakht318@gmail.com>
2018-01-14 15:41:09 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,45 +1,28 @@
 class Song
-  attr_accessor :name, :count, :artist, :genre
+  attr_accessor :name, :artist, :genre
 
   @@count = 0
   @@genres = []
   @@artists = []
-  @@genre_count = []
-  @@artist_count
 
-  def initialize (name, artist, genre)
-   @@count += 1
-   @@genres << genre
-   @@artists << artist
-   @name = name
-   @artist = artist
-   @genre = genre
- end
+  def initialize
+    @@count += 1
+    @@genres << genre
+    @@artists << artist
+    @genre = genre
+  
+  end
 
- def self.count
-  @@count
- end
+  def self.count
+    @@count
+  end
 
- def self.genres
-   @@genres.uniq!
- end
+  def self.genres
+    @@genres.uniq!
+  end
 
- def self.artists
-   @@artists.uniq!
- end
-
- def self.genre_count
-     @@genres.each do |genre|
-       @@genre_count[genre] = @@genres.count(genre)
-     end
-     @@genre_count
-end
-
- def self.artist_count
-   @@artists.each do |artist|
-     @@artist_count[artist] = @@artists.count(artist)
-   end
-   @@artist_count
- end
-
+  def self.artists
+    @@artists.uniq!
+  end
+    
 end
